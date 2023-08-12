9864bed71357c7b2cde715290567d1ea2a739cc2
MimHe <mimhe1381@gmail.com>
2023-08-12 15:38:10 +0330

    add forecast module

---
+++ src/utils/forecast.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch');
+
+const forecast = function (latitude, longitude, callback) {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset&current_weather=true&timezone=auto&forecast_days=1`;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                return callback('Unable to find location.', undefined);
+            }
+
+            return response.json();
+        })
+        .then(data => callback(undefined, data))
+        .catch(() => {
+            callback('Unable to fetch data.', undefined);
+        });
+}
+
+forecast(34.0536909, -118.242766, (error, data) => {
+    console.log(error)
+    console.log(data)
+});
+
+module.exports = forecast;
